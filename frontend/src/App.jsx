9e700a9f6b9fe7d044220d500a7d9e25ec97d5fe
Amila <amila@nordictravels.eu>
2026-08-18 15:13:15 +0300

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>I Do</h1>
+      <p>Smart to do App</p>
+    </div>
+  );
+}
+
+export default App;
